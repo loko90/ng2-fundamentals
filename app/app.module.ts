fac1b4c5c2d62e6ad521df5e3c06d1c07a4bc568
loko90 <loukoekemoer@gmail.com>
2018-03-29 18:56:04 +0200

    module 5 in progress

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavbarComponent } from './nav/navbar.component'
+import { EventService } from './events/shared/event.service'
 
 @NgModule({
 	imports: [
@@ -16,6 +17,7 @@ import { NavbarComponent } from './nav/navbar.component'
 		EventThumbnailComponent,
 		NavbarComponent
 	],
+	providers: [EventService,],
 	bootstrap: [
 		EventsAppComponent,
 	]
--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -9,9 +9,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
             <div>Date: {{event?.date}}</div>
             <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
                 Time: {{event?.time}}
-                <span *ngSwitchCase="'8:00 am'">Early Start</span>
-                <span *ngSwitchCase="'10:00 am'">Late Start</span>
-                <span *ngSwitchDefault>Normal Start</span>
+                <span *ngSwitchCase="'8:00 am'"> - Early Start</span>
+                <span *ngSwitchCase="'10:00 am'"> - Late Start</span>
+                <span *ngSwitchDefault> - Normal Start</span>
             </div>
             <div>Price: R{{event?.price}}</div>
             <div *ngIf="event?.location">
@@ -34,18 +34,18 @@ export class EventThumbnailComponent {
     @Input() event: any;
 
     getStartTimeClass() {
-        // 3 WAYS OF ACHIEVING THE SAME
+    //     // 3 WAYS OF ACHIEVING THE SAME
 
-        // 1
-        // const isEarlyStart = this.event && this.event.time === '8:00 am';
-        // return {green: isEarlyStart, bold: isEarlyStart};
+    //     // 1
+    //     // const isEarlyStart = this.event && this.event.time === '8:00 am';
+    //     // return {green: isEarlyStart, bold: isEarlyStart};
 
-        // 2
-        // if (this.event && this.event.time === '8:00 am')
-        //     return 'green bold';
-        // return '';
+    //     // 2
+    //     // if (this.event && this.event.time === '8:00 am')
+    //     //     return 'green bold';
+    //     // return '';
 
-        // 3
+    //     // 3
         if (this.event && this.event.time === '8:00 am')
             return ['green', 'bold'];
         return [];
